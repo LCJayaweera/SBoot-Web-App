811e880beb84e831168fa29f691a7e47380cfc09
Lakshan <lcjayaweera@gmail.com>
2023-07-17 13:22:44 +0900

    @RequestParam

--- src/main/java/com/lakjay/MyWebApp/HomeController.java
+++ src/main/java/com/lakjay/MyWebApp/HomeController.java
@@ -5,17 +5,18 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("home")
-    public String home(String name,HttpSession session){
+    public String home(@RequestParam("name") String myName, HttpSession session){
 
 
-        System.out.println("Hi "+name);
-        session.setAttribute("name",name);
+        System.out.println("Hi "+myName);
+        session.setAttribute("name",myName);
         return "home";
     }
 
